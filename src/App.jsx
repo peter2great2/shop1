eb0ae9a04e3ffd5e4ee5e9ec9040728996edff2e
Peter Sambo <peter2great2@gmail.com>
2025-01-09 15:36:17 +0100

    add Navbar component and implement routing with Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import Homepage from "./pages/Homepage";
 import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Login from "./pages/Login";
 import "./App.css";
 
 function App() {
    return (
       <>
-         <Homepage />
+         <BrowserRouter>
+            <Routes>
+               <Route
+                  path="/"
+                  element={<Homepage />}
+               />
+               <Route
+                  path="/login"
+                  element={<Login />}
+               />
+            </Routes>
+         </BrowserRouter>
       </>
    );
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import { NavbarSimple } from "../layouts/Navbar";
 const Homepage = () => {
-   return <div>Homepage</div>;
+   return (
+      <div>
+         <NavbarSimple />
+         Homepage
+      </div>
+   );
 };
 
 export default Homepage;
