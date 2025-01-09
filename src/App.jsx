7d9dca87c96e1bce27788318398e4c9570a943a1
Peter Sambo <peter2great2@gmail.com>
2025-01-09 12:23:30 +0100

    initialize React app with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Homepage from "./pages/Homepage";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+   return (
+      <>
+         <Homepage />
+      </>
+   );
+}
+
+export default App;
