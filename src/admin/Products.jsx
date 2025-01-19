21b6860999c55fa6a444679d00ba3f2e69f9e7d6
Peter Sambo <peter2great2@gmail.com>
2025-01-19 09:25:57 +0100

    fetch and display products in Admin Products page; enhance product listing with dynamic data

--- src/admin/Products.jsx
+++ src/admin/Products.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StickyNavbar } from "../layouts/Navbar";
 import sampleImage from "../assets/images/ei_1663268459705-removebg-preview.jpg";
+import axios from "axios";
 
 const AdminProductsPage = () => {
+   const [products, setProducts] = useState([]);
+   const [totalProducts, setTotalProducts] = useState(0);
+   useEffect(() => {
+      const fetchData = async () => {
+         await axios
+            .get("http://localhost:3000/api/products/all", {
+               withCredentials: true,
+            })
+            .then((res) => {
+               setProducts(res.data.products);
+               setTotalProducts(res.data.allProducts);
+            });
+      };
+      fetchData();
+   }, []);
    return (
       <div>
          <StickyNavbar />
          <div className="min-h-screen bg-gray-100 p-6">
             <header className="mb-8">
-               <h1 className="text-3xl font-bold text-gray-800">Products</h1>
+               <h1 className="text-3xl font-bold text-gray-800">{`Products ${totalProducts}`}</h1>
             </header>
 
             <div className="bg-white rounded-lg shadow-md p-6">
@@ -37,54 +53,56 @@ const AdminProductsPage = () => {
                      </tr>
                   </thead>
                   <tbody>
-                     <tr className="hover:bg-gray-100">
-                        <td className="p-4 border-b">1</td>
-                        <td className="p-4 border-b">
-                           <img
-                              src={sampleImage}
-                              alt="Sample Product"
-                              className="w-12 h-12 object-cover rounded-full"
-                           />
-                        </td>
-                        <td className="p-4 border-b">Television</td>
-                        <td className="p-4 border-b">Electronics</td>
-                        <td className="p-4 border-b">$10.00</td>
-                        <td className="p-4 border-b">50</td>
-                        <td className="p-4 border-b">
-                           <div className="flex space-x-2">
-                              <button className="px-3 py-1 text-white bg-blue-500 rounded-lg hover:bg-blue-600">
-                                 Edit
-                              </button>
-                              <button className="px-3 py-1 text-white bg-red-500 rounded-lg hover:bg-red-600">
-                                 Delete
-                              </button>
-                           </div>
-                        </td>
-                     </tr>
-                     <tr className="hover:bg-gray-100">
-                        <td className="p-4 border-b">2</td>
-                        <td className="p-4 border-b">
-                           <img
-                              src={sampleImage}
-                              alt="Another Product"
-                              className="w-12 h-12 object-cover rounded-full"
-                           />
-                        </td>
-                        <td className="p-4 border-b">Samsung Galaxy S21</td>
-                        <td className="p-4 border-b">Mobile Phones</td>
-                        <td className="p-4 border-b">$20.00</td>
-                        <td className="p-4 border-b">30</td>
-                        <td className="p-4 border-b">
-                           <div className="flex space-x-2">
-                              <button className="px-3 py-1 text-white bg-blue-500 rounded-lg hover:bg-blue-600">
-                                 Edit
-                              </button>
-                              <button className="px-3 py-1 text-white bg-red-500 rounded-lg hover:bg-red-600">
-                                 Delete
-                              </button>
-                           </div>
-                        </td>
-                     </tr>
+                     {products.map((product) => (
+                        <tr key={product._id}>
+                           <td>{product._id}</td>
+                           <td>
+                              <img
+                                 src={product.image}
+                                 alt="Sample Product"
+                                 className="w-12 h-12 object-cover rounded-full"
+                              />
+                           </td>
+                           <td className="p-4 hover:bg-gray-100 border-b">
+                              {product.name}
+                           </td>
+                           <td className="p-4 hover:bg-gray-100 border-b">
+                              {product.category}
+                           </td>
+                           <td className="p-4 hover:bg-gray-100 border-b">
+                              {product.price}
+                           </td>
+                           <td className="p-4 hover:bg-gray-100 border-b">
+                              {product.stock}
+                           </td>
+                           <td className="p-4 border-b">
+                              <div className="flex space-x-2">
+                                 <button
+                                    className="
+                                    px-3
+                                    py-1
+                                    text-white
+                                    bg-blue-500
+                                    rounded-lg
+                                    hover:bg-blue-600"
+                                 >
+                                    Edit
+                                 </button>
+                                 <button
+                                    className="
+                                    px-3
+                                    py-1
+                                    text-white
+                                    bg-red-400
+                                    rounded-lg
+                                    hover:bg-red-600"
+                                 >
+                                    Delete
+                                 </button>
+                              </div>
+                           </td>
+                        </tr>
+                     ))}
                   </tbody>
                </table>
             </div>
@@ -94,3 +112,28 @@ const AdminProductsPage = () => {
 };
 
 export default AdminProductsPage;
+
+// <tr className="hover:bg-gray-100">
+//    <td className="p-4 border-b">1</td>
+//    <td className="p-4 border-b">
+//       <img
+//          src={sampleImage}
+//          alt="Sample Product"
+//          className="w-12 h-12 object-cover rounded-full"
+//       />
+//    </td>
+//    <td className="p-4 border-b">Television</td>
+//    <td className="p-4 border-b">Electronics</td>
+//    <td className="p-4 border-b">$10.00</td>
+//    <td className="p-4 border-b">50</td>
+//    <td className="p-4 border-b">
+//       <div className="flex space-x-2">
+//          <button >
+//             Edit
+//          </button>
+//          <button className="px-3 py-1 text-white bg-red-500 rounded-lg hover:bg-red-600">
+//             Delete
+//          </button>
+//       </div>
+//    </td>
+// </tr>
