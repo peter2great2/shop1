2f21e8e524da22fe917a0ab6deee1904d8920b9a
Peter Sambo <peter2great2@gmail.com>
2025-02-04 13:01:48 +0100

    feat: implement add to cart functionality in ProductCard component; integrate API call and toast notifications

--- src/utils/Card.jsx
+++ src/utils/Card.jsx
@@ -1,6 +1,40 @@
 import React from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useContext } from "react";
+import { CartContext } from "../context/Cartcontext";
 
 const ProductCard = ({ product }) => {
+  const { cart, setCart } = useContext(CartContext);
+  const handleAddToCart = async (product) => {
+    console.log("Product added to cart: ", product);
+    await axios
+      .post(
+        "http://localhost:3000/api/orders/create",
+        {
+          items: [
+            {
+              product: product._id,
+              quantity: 1,
+              price: product.price,
+            },
+          ],
+          totalAmount: product.price,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        console.log("Product added to cart: ", res.data);
+        setCart([...cart, res.data]);
+
+        toast.success("Product added to cart");
+      })
+      .catch((err) => {
+        console.log("Error adding product to cart: ", err);
+      });
+  };
   return (
     <div className="w-48 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out overflow-hidden">
       {/* Product Image */}
@@ -21,7 +55,10 @@ const ProductCard = ({ product }) => {
         <p className="text-lg font-bold text-gray-900 mt-1">${product.price}</p>
 
         {/* Add to Cart Button */}
-        <button className="w-full mt-2 bg-blue-500 hover:bg-blue-600 text-white text-sm font-semibold py-1 px-2 rounded">
+        <button
+          className="w-full mt-2 bg-blue-500 hover:bg-blue-600 text-white text-sm font-semibold py-1 px-2 rounded"
+          onClick={() => handleAddToCart(product)}
+        >
           Add to Cart
         </button>
       </div>
